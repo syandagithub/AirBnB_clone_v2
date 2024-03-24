cb81d42764b99d09b9ddac17c2137e870fb3eea3
SYANDA MNTAMBO's GITHUB <112751864+syandagithub@users.noreply.github.com>
2024-03-24 20:04:13 +0200

    easytherepartner

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    Starts a Flask web application
+"""
+from flask import Flask, render_template
+from models import storage
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def handle_teardown(self):
+    """
+        method for teardown
+    """
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def state_list():
+    """
+        method to show states
+    """
+    states = storage.all('State').values()
+    return render_template("7-states_list.html", states=states)
+
+if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=5000)
